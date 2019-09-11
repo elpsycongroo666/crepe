febcc807f3e96180f0d321c13c61bc588bf7da9b
翔 <297962650@qq.com>
2019-09-11 21:32:26 +0800

    项目架构初始化

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <!-- 渲染头部组件 -->
-    <Header/>
+    <Header />
 
     <!-- 页面的占位符,类似于router-view -->
     <nuxt />
 
     <!-- 渲染页脚组件 -->
-    <Footer/>
+    <Footer />
   </div>
 </template>
 
@@ -19,7 +19,7 @@ import Header from "@/components/header";
 import Footer from "@/components/footer";
 
 export default {
-  
+
   // 注册组件
   components: {
     Header,
@@ -30,8 +30,8 @@ export default {
 
 <style >
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -39,27 +39,5 @@ html {
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
-}
-
-*{
-  margin:0;
-  padding:0;
-}
-
-ul,li,ol{
-  list-style: none;
-}
-
-i,em{
-  font-style: normal;
-}
-
-a{
-  color:inherit; 
-  text-decoration: none;
-}
-
-a:hover{
-  color:inherit; 
 }
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,35 +1,40 @@
-
+const path = require('path')
 export default {
   mode: 'universal',
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: '世界上最大的旅游网站',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', type: 'text/css', href: '//at.alicdn.com/t/font_1168872_ehvuah8v57g.css'}
+      { rel: 'icon', type: 'image/x-icon', href: '/images/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        type: 'text/css',
+        href: '//at.alicdn.com/t/font_1168872_ehvuah8v57g.css'
+      }
     ]
   },
   /*
-  ** Customize the progress-bar color
-  */
+   ** Customize the progress-bar color
+   */
   loading: { color: '#fff' },
   /*
-  ** Global CSS
-  */
-  css: [
-    'element-ui/lib/theme-chalk/index.css',
-    'assets/main.css'
-  ],
+   ** Global CSS
+   */
+  css: ['element-ui/lib/theme-chalk/index.css', 'assets/styles/main.less'],
   /*
-  ** Plugins to load before mounting the App
-  */
+   ** Plugins to load before mounting the App
+   */
   plugins: [
     '@/plugins/element-ui',
     // axios的拦截插件
@@ -38,33 +43,37 @@ export default {
     { src: '@/plugins/localStorage.js', ssr: false }
   ],
   /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-  ],
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [],
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
+    '@nuxtjs/axios'
   ],
   /*
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
   axios: {
-    baseURL: "http://127.0.0.1:1337" // 新增axios默认请求路径
+    baseURL: 'http://127.0.0.1:1337' // 新增axios默认请求路径
+  },
+  //配置别名
+  alias: {
+    img: path.join(__dirname, './assets/images/'),
+    components: path.join(__dirname, './components'),
+    styles: path.join(__dirname, './assets/styles')
   },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     transpile: [/^element-ui/],
     /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
 }
