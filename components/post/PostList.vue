63cf6dec8a4a1bcb0d5f7dc91c042252dffd9177
lerojon <leroy@qq.com>
2019-09-16 14:08:37 +0800

    旅游攻略2

--- components/post/PostList.vue
+++ components/post/PostList.vue
@@ -1,31 +1,29 @@
 <template>
   <div>
-    <div class="post-item">
+    <div class="post-item"
+         v-if=" data.images.length === 3">
+      <div class="post-content">
+        <h4 class="post-title">
+          <nuxt-link to="/post/detail">
+            {{data.title}}
+          </nuxt-link>
+        </h4>
 
-      <h4 class="post-title">{{data.title}}</h4>
-
-      <p class="post-desc">
-        <nuxt-link to="/">
-          {{data.summary}}
-        </nuxt-link>
-      </p>
-      <el-row class="post_card-images"
+        <p class="post-desc">
+          <nuxt-link to="/post/detail">
+            {{data.summary}}
+          </nuxt-link>
+        </p>
+      </div>
+      <el-row class="post_card-images post-cover"
               type="flex"
               justify="space-between">
-        <nuxt-link to="#"
+        <nuxt-link to="/post/detail"
                    v-for="(item,index) in data.images"
                    :key="index">
           <img :src="item"
                alt="">
         </nuxt-link>
-        <!-- <nuxt-link to="#">
-        <img src="@/images/u=3215222857,1774393785&fm=26&gp=0.jpg"
-             alt="">
-      </nuxt-link>
-      <nuxt-link to="#">
-        <img src="@/images/u=3215222857,1774393785&fm=26&gp=0.jpg"
-             alt="">
-      </nuxt-link> -->
       </el-row>
 
       <el-row class="post-info"
@@ -54,10 +52,64 @@
           {{data.like}}赞
         </span>
       </el-row>
-
-      <!-- 只有一张图片的样式 -->
-
     </div>
+
+    <!-- 只有一张图片的结构 -->
+    <div class="post-item-One"
+         v-else>
+      <el-row class="post-cover-one">
+        <nuxt-link to="/post/detail"
+                   v-for="(item,index) in data.images"
+                   :key="index">
+          <img :src="item"
+               alt="">
+        </nuxt-link>
+      </el-row>
+
+      <div class="post-content-one">
+        <h4 class="post-title-one">
+          <nuxt-link to="/post/detail">
+            {{data.title}}
+          </nuxt-link>
+        </h4>
+
+        <p class="post-desc-one">
+          <nuxt-link to="/post/detail">
+            {{data.summary}}
+          </nuxt-link>
+        </p>
+
+        <el-row class="post-info-one"
+                type="flex"
+                justify="space-between">
+          <el-row class="post-info-left-one"
+                  type="flex">
+            <span>
+              <i class="el-icon-location-outline"></i>{{data.cityName}}
+            </span>
+            <el-row class="post-user-one"
+                    type="flex">
+              by
+              <nuxt-link to="#">
+                <img :src="`${$axios.defaults.baseURL}${$store.state.user.userInfo.user.defaultAvatar}`"
+                     alt="">
+              </nuxt-link>
+              <nuxt-link to="#">
+                {{$store.state.user.userInfo.user.nickname}}
+              </nuxt-link>
+            </el-row>
+            <span>
+              <i class="el-icon-view"></i>{{data.watch}}
+            </span>
+          </el-row>
+          <span class="post-info-right-one">
+
+            {{data.like}}赞
+          </span>
+        </el-row>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -65,10 +117,14 @@
 export default {
   data () {
     return {
+      //只有一张图片的时候
+      OneImg: 'OnlyOneImg',
+
 
     }
   },
 
+  //------------------------------------------//
   props: {
     data: {
       type: Object,
@@ -161,6 +217,103 @@ export default {
 
   .post-info-right {
     color: orange;
+  }
+}
+
+//只有一张图片的时候的样式
+.post-item-One {
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  vertical-align: middle;
+  width: 100%;
+  padding: 20px 0;
+  border-bottom: 1px solid #eee;
+  align-items: center;
+  box-sizing: border-box;
+  justify-content: space-between;
+  display: flex;
+
+  .post-cover-one {
+    width: 220px;
+    height: 150px;
+    overflow: hidden;
+    flex-shrink: 0;
+    margin-right: 10px;
+    box-sizing: border-box;
+
+    &:before {
+      display: table;
+      content: "";
+    }
+
+    &:after {
+      clear: both;
+      display: table;
+      content: "";
+    }
+  }
+
+  .post-content-one {
+    width: 470px;
+
+    .post-title-one {
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      margin-bottom: 15px;
+      font-weight: 400;
+      font-size: 18px;
+    }
+
+    .post-desc-one {
+      margin-bottom: 15px;
+      line-height: 1.5;
+      font-size: 14px;
+      height: 63px;
+      overflow: hidden;
+      text-overflow: ellipsis;
+
+      a {
+        color: #666;
+      }
+    }
+
+    .post-info-left-one {
+      font-size: 12px;
+      color: #999;
+      align-items: center;
+      box-sizing: border-box;
+
+      .post-user-one {
+        margin-right: 10px;
+        align-items: center;
+        box-sizing: border-box;
+        vertical-align: middle;
+
+        img {
+          display: block;
+          width: 16px;
+          height: 16px;
+          border-radius: 100px;
+          margin: 5px;
+        }
+
+        a {
+          color: orange;
+        }
+      }
+
+      span {
+        margin-right: 10px;
+      }
+    }
+
+    .post-info-right-one {
+      color: orange;
+    }
   }
 }
 </style>
--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,51 +1,22 @@
 <template>
-<<<<<<< HEAD
   <div class="post_container">
     <el-row class="el-row"
             type="flex"
             justify="space-between">
-      <!-- 右边导航栏 -->
-      <div class="post_menus-wrapper"></div>
+      <!-------------------------------------------- 右边导航栏 ---------------------------------------------->
+      <div class="post_menus-wrapper">
+        <PostMenusWrapper />
+      </div>
 
-      <!-- 左边文章内容 -->
+      <!-------------------------------------------- 左边文章内容 ---------------------------------------------->
       <div class="post-wrapper">
-        <div class="post_search-wrapper">
-=======
-  <div style="padding:50px;">
-    <button>
-      <nuxt-link to="/post/create">写游记</nuxt-link>
-    </button>
-    <h2>组件递归</h2>
 
-    <Item :data="list" />
->>>>>>> 17e4f1c399a494006a1a903887d01189e8a5387d
-
-          <!-- 搜索框 -->
-          <el-row class="post_search-bar"
-                  type="flex"
-                  justify="space-between">
-            <input type="text"
-                   placeholder="请输入想去的地方，比如：'广州'"
-                   v-model="post_search" />
-            <i class="el-icon-search"></i>
-          </el-row>
-          <div class="post_search-recommend">
-            推荐：
-            <span>广州</span>
-            <span>上海</span>
-            <span>北京</span>
-          </div>
-        </div>
+        <!-- 搜索框和推荐城市 -->
+        <PostSearch :data="cachePostDataList"
+                    @setPostDataList="setPostDataList" />
 
         <!-- 文章标题 -->
-        <el-row class="post-title"
-                type="flex"
-                justify="space-between">
-          <h4>推荐攻略</h4>
-          <el-button type="primary"
-                     icon="el-icon-edit"
-                     class="post_el-button">写游记</el-button>
-        </el-row>
+        <PostTitle />
 
         <!-- 文章列表 -->
         <div class="post-list">
@@ -73,27 +44,41 @@
 </template>
 
 <script>
-<<<<<<< HEAD
 //引入攻略组件
-import PostList from "@/components/post/PostList.vue";
+import PostList from "@/components/post/PostList.vue";      //文章列表
+import PostSearch from "@/components/post/PostSearch.vue";  //搜索框和推荐城市
+import PostTitle from "@/components/post/PostTitle.vue";  //文章标题
+import PostMenusWrapper from "@/components/post/PostMenusWrapper.vue"; //右边的导航
 
+//--------------------------------------------//
 export default {
   components: {
-    PostList
+    PostList,
+    PostSearch,
+    PostTitle,
+    PostMenusWrapper
   },
+
+
+  //----------------------------------------//
   data () {
     return {
       pageIndex: 1,
-      pageSize: 5,
+      pageSize: 2,
       total: 0,
-      PostData: {
-
-      },
-      post_search: '',
-      PostDataList: []
+      start: 1,
+      limit: 2,
+      city: '',
+      PostData: {},
+      // post_search: '',
+      PostDataList: [],
+      //缓存文章列表数据
+      cachePostDataList: {},
     };
   },
 
+
+  //----------------------------------------//
   watch: {
     // watch可以监听this下的所有属性
     $route () {
@@ -102,36 +87,108 @@ export default {
     }
   },
 
+
+
+
+
+  //------------------------------------//
   methods: {
+
+
+
+
+
+
     //请求文章数据列表
     getPostData () {
-      this.$axios({
-        url: 'posts'
-        // params: this.$route.query
-      }).then(res => {
-        console.log(res)
-        this.PostData = res.data
-        this.total = res.data.total
-        this.PostDataList = this.PostData.data
-      })
+      this.getDataAx();
+      // this.$axios({
+      //   url: 'posts',
+      //   params: {
+      //     _start: 1,
+      //     _limit: this.pageSize
+      //   }
+      //   // params: this.$route.query
+      // }).then(res => {
+      //   console.log(res)
+      //   this.PostDataList = res.data.data;
+      //   // this.cachePostDataList = { ...res.data };
+      //   // this.PostDataList = this.PostData.data;
+      //   this.total = res.data.total;
+      //   // this.PostDataList = this.PostData.data.slice((this.pageIndex - 1) * this.pageSize, this.pageIndex * this.pageSize);
+      // })
+
     },
 
 
+
+    //过滤城市
+    setPostDataList (CityData) {
+      this.city = CityData;
+      this.getDataAx();
+      //   // console.log(999)
+      //   // console.log(Newarr)
+      //   // this.PostDataList = Newarr;
+      //   // this.total = Newarr.length;
+      //   // this.PostDataList = Newarr.slice((this.pageIndex - 1) * this.pageSize, this.pageIndex * this.pageSize);
+    },
+
+    //分页的点击事件
     handleSizeChange (val) {
       console.log(`每页 ${val} 条`);
-      this.pageSize = val;
-      this.PostDataList = this.PostData.data.slice(0, val);
+      this.limit = val;
+
+      this.getDataAx();
+      // this.PostDataList = this.PostData.data.slice(0, val);
+
+
     },
     handleCurrentChange (val) {
       console.log(`当前页: ${val}`);
-      this.pageIndex = val;
-      this.PostDataList = this.PostData.data.slice((this.pageIndex - 1) * this.pageSize, this.pageIndex * this.pageSize)
+      this.start = val;
+      this.getDataAx();
+      // console.log(this._start)
+      // this.PostDataList = this.PostData.data.slice((this.pageIndex - 1) * this.pageSize, this.pageIndex * this.pageSize);
+      // this.$axios({
+      //   url: "posts",
+      //   params: {
+      //     city: '',
+      //     _start: val,
+      //     _limit: val
+      //   }
+      // })
     },
 
 
+
+    //封装一个接口请求
+    getDataAx () {
+      let obj = {
+        _start: this.start,
+        _limit: this.limit
+      }
+      if (this.city) {
+        obj.city = this.city
+      }
+
+      this.$axios({
+        url: "posts",         //+ "?_start=" + this._start + "&limit=" + this._limit,
+        params: obj
+      }).then(res => {
+        console.log(res.data.data.cityName)
+        this.PostData = res.data
+        this.PostDataList = this.PostData.data
+        this.total = res.data.total
+        this.city = res.data.data.cityName
+      })
+    }
+
   },
 
+  //--------------------------------//
   mounted () {
+    console.log(this._start);
+
     this.getPostData()
   }
 
@@ -166,97 +223,6 @@ export default {
   width: 700px;
 }
 
-.post_search-wrapper {
-  .post_search-bar {
-    width: 100%;
-    box-sizing: border-box;
-    height: 40px;
-    line-height: 40px;
-    border: 3px solid orange;
-    align-items: center;
-
-    input {
-      flex: 1;
-      padding: 0 20px;
-      line-height: 40px;
-      outline: none;
-      border: none;
-      background: none;
-    }
-
-    i {
-      font-size: 24px;
-      color: orange;
-      font-weight: 700;
-      margin-right: 10px;
-    }
-  }
-
-  .post_search-recommend {
-    padding: 10px 0;
-    font-size: 12px;
-    color: #666;
-
-    span {
-      margin-right: 5px;
-      &:hover {
-        border-bottom: 1px solid orange;
-        color: orange;
-        cursor: pointer;
-      }
-    }
-  }
-}
-
-.post-title {
-  padding-bottom: 10px;
-  border-bottom: 1px solid #eee;
-  position: relative;
-  align-items: center;
-
-  h4 {
-    font-weight: 400;
-    font-size: 18px;
-    color: orange;
-
-    &:after {
-      display: block;
-      content: "";
-      width: 72px;
-      height: 2px;
-      background: orange;
-      position: absolute;
-      bottom: 0;
-      left: 0;
-    }
-  }
-
-  .post_el-button {
-    display: inline-block;
-    line-height: 1;
-    white-space: nowrap;
-    cursor: pointer;
-    background: #fff;
-    border: 1px solid #dcdfe6;
-    text-align: center;
-    box-sizing: border-box;
-    outline: 0;
-    margin: 0;
-    transition: 0.1s;
-    font-weight: 500;
-    padding: 12px 20px;
-    font-size: 14px;
-    border-radius: 4px;
-    color: #fff;
-    background-color: #409eff;
-    border-color: #409eff;
-
-    &:hover {
-      background: rgb(102, 177, 255);
-    }
-  }
-}
-
 // 分页
 el-pagination {
   white-space: nowrap;
@@ -264,49 +230,4 @@ el-pagination {
   color: #303133;
   font-weight: 700;
 }
-=======
-export default {
-  data () {
-    return {
-      list: [
-        {
-          title: "衣服",
-          children: [
-            {
-              title: "男装",
-              children: [
-                { title: "秋裤" },
-                {                  title: "西装", children: [
-                    { title: "领带" },
-                    { title: "皮带" },
-                  ]                }
-              ]
-            },
-            {
-              title: "女装",
-              children: [
-                { title: "裙子" },
-                { title: "比基尼" }
-              ]
-            }
-          ]
-        },
-
-        {
-          title: "电器",
-          children: [
-            { title: "冰箱" },
-            { title: "空调" },
-            { title: "游戏机" }
-          ]
-        }
-      ]
-    }
-  },
-
-}
-</script>
-
-<style>
->>>>>>> 17e4f1c399a494006a1a903887d01189e8a5387d
 </style>
